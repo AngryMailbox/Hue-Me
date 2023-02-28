b11e6193fb8d9e514e86e5ada66269baa078dceb
AngryMailbox <mans@klintan.net>
2023-02-28 09:44:14 +0100

    Added venv and downgrade to python 3.7, bpm detection working

--- hueControl.py
+++ hueControl.py
@@ -1,7 +1,9 @@
 import time
 import phue
 from audioplayer import AudioPlayer
-import tempocnn
+import librosa
+import numpy
+#import tempocnn
 
 
 #if file bridgeIp is empty, get bridge ip and store in file
@@ -21,8 +23,12 @@ except:
 # Connect to the bridge
 b = phue.Bridge(''+bridgeIp)
 
-# If the app is not registered and the button is not pressed, press the button and rerun connect()
-b.connect()
+# If the app is not registered the app must be rerun in order to register the app with the bridge
+try:
+    b.connect()
+except:
+    print("Press the button on the bridge and rerun the program")
+    exit(1)
 
 # Get the bridge state (returns full dictionary)
 b.get_api()
@@ -38,5 +44,24 @@ print(b.get_light_objects('id'))
 #turns on "Vägglampa 1"
 b.set_light('Vägglampa 1', 'on', True)
 
-#play audio from file "metro.mp3"
+#plays audio from "metro.mp3"
 AudioPlayer("metro.mp3").play(block=True)
+
+y, sr = librosa.load("metro.mp3")
+tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+print("Estimated tempo: {:.2f} BPM".format(tempo))
+
+#blink light at tempo
+try:
+    while True:
+        b.set_light('Vägglampa 1', 'on', True)
+        print("on")
+        time.sleep(60/tempo)
+        b.set_light('Vägglampa 1', 'on', False)
+        print("off")
+        time.sleep(60/tempo)
+#if keyboard interrupt, turn off light and exit
+except:
+    b.set_light('Vägglampa 1', 'on', False)
+
+exit()
